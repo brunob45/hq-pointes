cb791cfa0db085d53218149558379aa3fdde413b
Bruno Bousquet <21108660+brunob45@users.noreply.github.com>
2025-01-07 23:18:10 -0500

    add comments

--- main.py
+++ main.py
@@ -11,22 +11,30 @@ from time import sleep
 
 
 class Event:
+  ''' event description '''
+
   def from_hq(self, data):
+    ''' create event from Hydro-Quebec API '''
+
     self.start = datetime.fromisoformat(data["datedebut"])
     self.end = datetime.fromisoformat(data["datefin"])
     self.summary = "pointe"
     return self
 
   def from_ha(self, data):
+    ''' create event from Home Assistant API '''
+
     self.start = datetime.fromisoformat(data["start"]["dateTime"])
     self.end = datetime.fromisoformat(data["end"]["dateTime"])
     self.summary = data["summary"]
     return self
 
   def __repr__(self) -> str:
+    ''' text representation '''
     return json.dumps(self.get_data(), indent=2)
 
   def __eq__(self, value):
+    ''' test if events are equal '''
     return (
       isinstance(value, Event)
       and (self.start == value.start)
@@ -35,6 +43,7 @@ class Event:
     )
 
   def get_data(self) -> dict:
+    ''' create the json object to create a new Home Assistant event '''
     return {
       "entity_id": "calendar.hq_flex_d",
       "summary": "pointe",
@@ -44,6 +53,7 @@ class Event:
 
 
 def get_ha_events(url: str, headers: dict):
+  ''' fetch events from Home Assistant '''
   start = datetime.now()
   end = start + timedelta(days=7)
 
@@ -64,6 +74,8 @@ def get_ha_events(url: str, headers: dict):
 
 
 def get_hq_events():
+  ''' fetch events from Hydro-Quebec '''
+  # https://donnees.hydroquebec.com/explore/dataset/evenements-pointe/information/
   url = "https://donnees.hydroquebec.com/api/explore/v2.1/catalog/datasets/evenements-pointe/records?where=offre%3D%22CPC-D%22&order_by=datedebut%20desc&limit=20"
   response = requests.get(url)
 
@@ -77,6 +89,8 @@ def get_hq_events():
 
 
 def create_ha_event(url: str, headers: dict, data: dict):
+  ''' create a new event in Home Assistant '''
+  # https://www.home-assistant.io/integrations/calendar/#service-calendarcreate_event
   url = f"{url}/api/services/calendars/create_event"
   response = requests.get(url, headers=headers, json=data)
   print(response.text)
@@ -88,20 +102,22 @@ def compare_events(url: str, headers: dict):
   events_ha = get_ha_events(HA_URL, HEADERS)
   events_hq = get_hq_events()
 
+  # for all Hydro-Quebec events
   for event in events_hq:
-    if event.start < NOW:
+    if event.start < NOW:  # ignore if in the past
       print(f'Event ignored {event}')
-    elif any(event == x for x in events_ha):
+    elif any(event == x for x in events_ha):  # compare with all HA events, ignore if already present
       print(f'Event found {event}')
-    else:
+    else:  # new event, create in HA
       create_ha_event(HA_URL, HEADERS, event.get_data())
       print(f'Event created {event}')
 
 
-
 if __name__ == '__main__':
+  ''' main function '''
   load_dotenv()
 
+  # https://developers.home-assistant.io/docs/api/rest/
   HA_URL = os.getenv('HA_URL')
   HEADERS = {
     "Authorization": f"Bearer {os.getenv('HA_TOKEN')}",
